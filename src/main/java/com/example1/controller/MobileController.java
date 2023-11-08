e0a0753da2b6adeb5bb31552fef5f1db77c4103a
Eeshita11 <eeshitatalasila45@gmail.com>
2023-11-08 18:05:46 +0530

    commit-msg

---
+++ src/main/java/com/example1/controller/MobileController.java
@@ -0,0 +1,61 @@
+package com.example1.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example1.model.Mobile;
+import com.example1.service.MobileJpaService;
+//import com.example1.service.MobileMYSQLService;
+//import com.example1.service.MobileService;
+
+@RestController
+public class MobileController 
+{
+	//MobileService mobileService=new MobileService();
+	
+	//@Autowired
+	//private MobileMYSQLService mobileService;
+	
+	@Autowired
+	private MobileJpaService mobileService;
+	
+	
+	@GetMapping("/mobiles")
+	public ArrayList<Mobile> getMobiles() 
+	{
+		return mobileService.getMobiles();
+	}
+	
+	@GetMapping("mobiles/{mobileId}")
+	public Mobile getMobileById(@PathVariable("mobileId")int mobileId) 
+	{
+		return mobileService.getMobileById(mobileId);
+	}
+	
+     @PostMapping("/mobiles/addnewmobile")
+	  public Mobile addMobile(@RequestBody Mobile mobile)
+	  {
+		return mobileService.addMobile(mobile);
+	  }
+
+	
+	@PutMapping("mobiles/{mobileId}")
+	public Mobile updateMobile(@PathVariable("mobileId") int mobileId, @RequestBody Mobile mobile) 
+	{
+		return mobileService.updateMobile(mobileId, mobile);
+	}
+	
+	@DeleteMapping("mobiles/{mobileId}")
+	public void deleteMobile(@PathVariable("mobileId") int mobileId)
+	{
+		mobileService.deleteMobile(mobileId);
+	}
+}
---
+++ src/main/java/com/example1/repository/MobileRepository.java
@@ -0,0 +1,15 @@
+package com.example1.repository;
+
+import java.util.ArrayList;
+import com.example1.model.Mobile;
+
+public interface MobileRepository
+{
+	ArrayList<Mobile> getMobiles();
+	Mobile getMobileById(int mobileId);
+	Mobile addMobile(Mobile mobile);
+	Mobile updateMobile(int mobileId, Mobile mobile);
+	void deleteMobile(int mobileId);
+	
+}
+
